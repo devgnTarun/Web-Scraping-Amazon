fb66740d5cb29defe7fd82bda059a08538e193f2
Tarun Devgan <107904243+devgnTarun@users.noreply.github.com>
2023-12-23 23:43:15 +0530

    changed group

--- lib/bot/index.ts
+++ lib/bot/index.ts
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot("6381538863:AAHzq7B9WVd7BDfd8IKu7D9HcZe_bczcdcA");
 
-const chatId = -1001750751576;
+const chatId = -1002013238169;
 
 export const shareProduct = async (image: string, telegramData: String) => {
     try {
